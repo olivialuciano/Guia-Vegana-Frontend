19fee5e224e6bb8aec356e05a0c25683db54e1fd
Olivia <olivialuciano42@gmail.com>
2025-02-16 17:53:26 -0300

    Correccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
