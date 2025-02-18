347b5a6b89d55785b653a71ab8942871d673d965
Lucia Palazzini <lulipalazzini@gmail.com>
2025-02-18 15:35:10 -0300

    Pagina profesionales de la salud y cards

--- src/pages/HealthProfessional/HealthProfessional.jsx
+++ src/pages/HealthProfessional/HealthProfessional.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./HealthProfessional.css";
 
 const HealthProfessional = () => {
-  return <div>HealthProfessional</div>;
+  const [healthProfessionals, setHealthProfessionals] = useState([]);
+
+  useEffect(() => {
+    fetch("https://guiavegana.somee.com/api/HealthProfessional")
+      .then((response) => response.json())
+      .then((data) => setHealthProfessionals(data))
+      .catch((error) =>
+        console.error("Error fetching health professionals:", error)
+      );
+  }, []);
+
+  return (
+    <div className="health-container">
+      <div className="header">
+        <h1 className="header-title">Profesionales de la Salud</h1>
+      </div>
+      <div className="health-list">
+        {healthProfessionals.map((professional) => (
+          <div key={professional.id} className="health-card">
+            <img
+              src={professional.image}
+              alt={professional.name}
+              className="health-image"
+            />
+            <h2 className="health-name">{professional.name}</h2>
+            <p className="health-specialty">
+              Especialidad: {professional.specialty}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HealthProfessional;
