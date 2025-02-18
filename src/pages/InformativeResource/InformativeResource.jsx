9b13e78aa548fca42c4dc54524e23ecc0754a7dd
Lucia Palazzini <lulipalazzini@gmail.com>
2025-02-18 15:54:43 -0300

    Pagina recursos informativos y cards

--- src/pages/InformativeResource/InformativeResource.jsx
+++ src/pages/InformativeResource/InformativeResource.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./InformativeResource.css";
 
-const InformativeResource = () => {
-  return <div>InformativeResource</div>;
+const InformativeResources = () => {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    fetch("https://guiavegana.somee.com/api/InformativeResource")
+      .then((response) => response.json())
+      .then((data) => setResources(data))
+      .catch((error) =>
+        console.error("Error fetching informative resources:", error)
+      );
+  }, []);
+
+  return (
+    <div className="info-container">
+      <div className="header">
+        <h1 className="header-title">Recursos informativos</h1>
+      </div>
+      <div className="info-list">
+        {resources.map((resource) => (
+          <div key={resource.id} className="info-card">
+            <img
+              src={resource.image}
+              alt={resource.name}
+              className="info-image"
+            />
+            <h2 className="info-name">{resource.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default InformativeResource;
+export default InformativeResources;
