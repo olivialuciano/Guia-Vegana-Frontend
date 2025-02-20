66aa306aa827eb10b996c65bb8a4e8674cdb012f
Olivia <olivialuciano42@gmail.com>
2025-02-20 15:49:59 -0300

    pagina de not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css"; // Importamos el CSS externo
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found-container">
+      <h1 className="error-code">404</h1>
+      <p className="error-text">Oops... Página no encontrada</p>
+      <p className="error-message">
+        Parece que la página que buscas no existe.
+      </p>
+      <Link to="/" className="home-button">
+        Volver al inicio
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
