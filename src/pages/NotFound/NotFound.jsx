326d497e76c30830b2b3588c6e7896c05d02c06c
Olivia <olivialuciano42@gmail.com>
2025-06-19 18:50:11 -0300

    estilizado

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -7,9 +7,7 @@ const NotFound = () => {
     <div className="not-found-container">
       <h1 className="error-code">404</h1>
       <p className="error-text">Oops... Página no encontrada</p>
-      <p className="error-message">
-        Parece que la página que buscas no existe.
-      </p>
+
       <Link to="/" className="home-button">
         Volver al inicio
       </Link>
